cca64e32ead9a07c76864ecfc72bbe7b5f4b3184
awak3n-moon <78362865+awak3n-moon@users.noreply.github.com>
2021-02-28 03:23:23 +0530

    final working model without gui

--- model_creation.py
+++ model_creation.py
@@ -7,68 +7,58 @@ import tflearn
 import tensorflow
 import random
 import json
-import pickle
-import pandas as pd
-#import speech_recognition as sr
-
-data = pd.read_json("intents.json")
 
 
-#with open("intents.json") as file:
-    #data = json.load(file)
-
-try:
-    with open("data.pickle", "rb") as f:
-        words, labels, training, output = pickle.load(f)
-except:
-    words = []
-    labels = []
-    docs_x = []
-    docs_y = []
-
-    for intent in data["intents"]:
-        for pattern in intent["patterns"]:
-            wrds = nltk.word_tokenize(pattern)
-            words.extend(wrds)
-            docs_x.append(wrds)
-            docs_y.append(intent["tag"])
-
-        if intent["tag"] not in labels:
-            labels.append(intent["tag"])
-
-    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-    words = sorted(list(set(words)))
-
-    labels = sorted(labels)
-
-    training = []
-    output = []
-
-    out_empty = [0 for _ in range(len(labels))]
-
-    for x, doc in enumerate(docs_x):
-        bag = []
-
-        wrds = [stemmer.stem(w.lower()) for w in doc]
-
-        for w in words:
-            if w in wrds:
-                bag.append(1)
-            else:
-                bag.append(0)
-
-        output_row = out_empty[:]
-        output_row[labels.index(docs_y[x])] = 1
-
-        training.append(bag)
-        output.append(output_row)
+with open("intents.json") as file:
+    data = json.load(file)
 
 
-    training = numpy.array(training)
-    output = numpy.array(output)
 
-    with open("data.pickle", "wb") as f:
-        pickle.dump((words, labels, training, output), f)
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w.lower()) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+
+training = numpy.array(training)
+output = numpy.array(output)
 
 #tensorflow.reset_default_graph()
 from tensorflow.python.framework import ops
@@ -82,11 +72,8 @@ net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-try:
-    model.load("model.tflearn")
-except:
-    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-    model.save("model.tflearn")
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
 
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
@@ -118,6 +105,5 @@ def chat():
                 responses = tg['responses']
 
         print(random.choice(responses))
-
 
 chat()
