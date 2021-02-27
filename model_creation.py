61720a802c3248109109fa26b5ec3cb517322588
awak3n-moon <78362865+awak3n-moon@users.noreply.github.com>
2021-02-27 13:48:17 +0530

    Only model training.

--- model_creation.py
+++ model_creation.py
@@ -12,60 +12,53 @@ import json
 with open("intents.json") as file:
     data = json.load(file)
 
-try:
-    with open("data.pickle","rb") as f:
-        words, labels, training, output = pickle.load(f)
-
-except:
-        
-    words = []
-    labels = []
-    docs_x = []
-    docs_y = []
-
-    for intent in data["intents"]:
-        for pattern in intent["patterns"]:
-            wrds = nltk.word_tokenize(pattern)
-            words.extend(wrds)
-            docs_x.append(wrds)
-            docs_y.append(intent["tag"])
-
-        if intent["tag"] not in labels:
-            labels.append(intent["tag"])
-
-    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-    words = sorted(list(set(words)))
-
-    labels = sorted(labels)
-
-    training = []
-    output = []
-
-    out_empty = [0 for _ in range(len(labels))]
-
-    for x, doc in enumerate(docs_x):
-        bag = []
-
-        wrds = [stemmer.stem(w.lower()) for w in doc]
-
-        for w in words:
-            if w in wrds:
-                bag.append(1)
-            else:
-                bag.append(0)
-
-        output_row = out_empty[:]
-        output_row[labels.index(docs_y[x])] = 1
-
-        training.append(bag)
-        output.append(output_row)
 
 
-    training = numpy.array(training)
-    output = numpy.array(output)
+words = []
+labels = []
+docs_x = []
+docs_y = []
 
-    with open("data.pickle","rb") as f:
-        pickle.dump(words, labels, training, output), f))
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w.lower()) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+
+training = numpy.array(training)
+output = numpy.array(output)
 
 #tensorflow.reset_default_graph()
 from tensorflow.python.framework import ops
@@ -78,8 +71,5 @@ net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
 net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
-
-try:
-    model.load("model.tflearn")
-    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)\
-    model.save("model.tflearn")
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
