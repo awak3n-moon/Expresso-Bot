278c4a5d7063aea412e7bad3f7737d84e6de28c0
awak3n-moon <78362865+awak3n-moon@users.noreply.github.com>
2021-02-28 04:05:17 +0530

    added functional gui

--- model_creation.py
+++ model_creation.py
@@ -72,7 +72,7 @@ net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.fit(training, output, n_epoch=100, batch_size=8, show_metric=True)
 model.save("model.tflearn")
 
 def bag_of_words(s, words):
@@ -88,22 +88,59 @@ def bag_of_words(s, words):
             
     return numpy.array(bag)
 
+import tkinter
+from tkinter import *
 
-def chat():
-    print("Start talking with the bot (type quit to stop)!")
-    while True:
-        inp = input("You: ")
-        if inp.lower() == "quit":
-            break
 
-        results = model.predict([bag_of_words(inp, words)])
-        results_index = numpy.argmax(results)
-        tag = labels[results_index]
+def send():
+    msg = EntryBox.get("1.0",'end-1c')
+    EntryBox.delete("0.0",END)    
 
-        for tg in data["intents"]:
-            if tg['tag'] == tag:
-                responses = tg['responses']
+    results = model.predict([bag_of_words(msg, words)])
+    results_index = numpy.argmax(results)
+    tag = labels[results_index]
 
-        print(random.choice(responses))
+    for tg in data["intents"]:
+        if tg['tag'] == tag:
+            responses = tg['responses']
 
-chat()
+    if msg != '':
+        ChatLog.config(state=NORMAL)
+        ChatLog.insert(END, "You: " + msg + '\n\n')
+        ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+
+        res = random.choice(responses)
+        ChatLog.insert(END, "Bot: " + res + '\n\n')
+
+        ChatLog.config(state=DISABLED)
+        ChatLog.yview(END)
+
+base = Tk()
+base.title("Chat-Bot")
+base.geometry("600x500")
+base.resizable(width=FALSE, height=FALSE)
+
+
+ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+
+ChatLog.config(state=DISABLED)
+
+
+scrollbar = Scrollbar(base, command=ChatLog.yview,cursor="arrow")
+ChatLog['yscrollcommand'] = scrollbar.set
+
+
+SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
+                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
+                    command= send )
+
+
+EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
+
+
+scrollbar.place(x=576,y=6, height=386)
+ChatLog.place(x=6,y=6, height=386, width=570)
+EntryBox.place(x=128, y=401, height=90, width=448)
+SendButton.place(x=6, y=401, height=90)
+
+base.mainloop()
